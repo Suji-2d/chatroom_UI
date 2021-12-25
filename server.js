f640f601d5d2168f5c70c99cd502a3af3c9ca9f7
Suji-2d <67060938+Suji-2d@users.noreply.github.com>
2021-12-25 18:56:50 +0530

    herohu config13

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/chat-room'));
+app.use(express.static('./dist/Chat-room'));
 
 app.get('/*', (req, res) => {
-    res.sendFile('index.html',{root : 'dist/chat-room/'}); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile('index.html',{root : 'dist/Chat-room/'}); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 // Start the app by listening on the default Heroku port
